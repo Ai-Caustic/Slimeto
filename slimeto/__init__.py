4a30a1660c9790f7c45be077311eacca3fb6467e
Ai-Caustic <victormichenim@gmail.com>
2023-08-29 16:57:56 +0300

    Created DB and added login and register pages and functionality

--- slimeto/__init__.py
+++ slimeto/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
+from flask_login import LoginManager
 from .extensions import db
 from.main.routes import main
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret_key'
+    app.config['SESSION_TYPE'] = 'memcached'
     app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'main.login'
+    login_manager.init_app(app)
     
-    from .models import models
+    from .models.User import User
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     with app.app_context():
         db.create_all()
--- slimeto/main/routes.py
+++ slimeto/main/routes.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template, request, url_for, redirect
-from ..models import models
+from flask import Blueprint, render_template, request, url_for, redirect, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from ..models.User import User
 from ..extensions import db
+from flask_login import login_required, current_user, login_user, logout_user
+
 
 main = Blueprint('main', __name__)
 
@@ -8,3 +11,56 @@ main = Blueprint('main', __name__)
 def index():
     
     return render_template('index.html')
+
+@main.route('/register')
+def register():
+    
+    return render_template('register.html')
+
+@main.route('/register', methods=['POST'])
+def register_post():
+    first_name = request.form.get('first-name')
+    last_name = request.form.get('last-name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    user = User.query.filter_by(email=email).first()
+    if user:
+        flash("Email address already exists")
+        
+        return redirect(url_for('main.register'))
+    
+    new_user = User(
+        first_name = first_name,
+        last_name = last_name,
+        email = email,
+        password = generate_password_hash(password, method='sha256')
+    )
+    
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return redirect(url_for('main.login'))
+
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@main.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')    
+    user = User.query.filter_by(email=email).first() 
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again')
+        return redirect(url_for('main.login'))
+    login_user(user)
+    return redirect(url_for('main.index'))
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.login'))
